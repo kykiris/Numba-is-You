a5074abf236dd2849e36d997d2b3d640843ee92e
kykiris <kykiris@snu.ac.kr>
2024-06-09 23:57:58 +0900

    library including header in Game.hpp is deleted.

--- Game.hpp
+++ Game.hpp
@@ -1,7 +1,7 @@
 #pragma once
 #include <memory>
 #include <deque>
-#include <iostream>
+// #include <iostream>
 
 #include "Map.hpp"
 #include "Enums.hpp"
--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 
 int main(int argc, char* argv[])
 {
-    std::string level_name = (argc > 1) ? argv[1] : "Test-p17-18.txt";
+    std::string level_name = (argc > 1) ? argv[1] : "levels/7-2p-item-in-wall.txt";
     Game game{ level_name };
     game.Run();
 }
